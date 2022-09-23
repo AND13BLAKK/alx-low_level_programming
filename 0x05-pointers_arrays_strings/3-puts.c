0097d69cb151a397054e9b9915835d8e83e91a64
AND13BLAKK <andieblakk13@gmail.com>
2022-09-23 00:31:29 -0700

    zamali

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _puts - prints a string followed by a new line..
- * 
+ * _puts - prints a string followed by a new line
+ *
  *@str: the string to print.
- *  
+ * 
  */
 
 void _puts(char *str)
